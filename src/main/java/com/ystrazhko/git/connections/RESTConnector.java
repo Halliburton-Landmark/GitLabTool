bf52dc755e97ce5fe0243effab783ec2e71cd320
Lyska Lyudmila <lucy.daehyun@gmail.com>
2017-02-23 18:28:32 +0200

    Created new branch. Get list of groups for signed user.
    Added group service. 
    Changed sendPost method from RESTConnectorImpl to handle all types of queries.

--- src/main/java/com/ystrazhko/git/connections/RESTConnector.java
+++ src/main/java/com/ystrazhko/git/connections/RESTConnector.java
@@ -5,5 +5,5 @@ import java.util.Map;
 public interface RESTConnector {
     String URL_MAIN_PART = "https://gitlab.com/api/v3";
 
-    Object sendPost(String url, Map<String, String> params);
+    Object sendPost(String url, Map<String, String> params, Map<String, String> header, String reguest);
 }
--- src/main/java/com/ystrazhko/git/services/LoginServiceImpl.java
+++ src/main/java/com/ystrazhko/git/services/LoginServiceImpl.java
@@ -8,6 +8,7 @@ public class LoginServiceImpl implements LoginService {
 
     private RESTConnector _connector;
 
+
     public LoginServiceImpl(RESTConnector connector) {
         setConnector(connector);
     }
@@ -17,7 +18,12 @@ public class LoginServiceImpl implements LoginService {
         HashMap<String, String> params = new HashMap<>();
         params.put("login", name);
         params.put("password", password);
-        return getConnector().sendPost("/session", params);
+        String json = (String) getConnector().sendPost("/session", params, null, "POST");
+
+        ((GroupsUserService) ServiceProvider.getInstance().getService
+                  (GroupsUserServiceImpl.class.getName())).getGroups(json);
+
+        return json;
     }
 
     private RESTConnector getConnector() {
