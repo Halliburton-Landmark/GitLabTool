ae8e0237cf57a8a5a2af5ab1be1d0b0433f8d171
Pavel Podgorniy <podgpavel@gmail.com>
2017-04-05 18:22:57 +0300

    Xml MapAdapter changed for working with Group instead groupId

--- src/main/java/com/ystrazhko/git/xml/MapAdapter.java
+++ src/main/java/com/ystrazhko/git/xml/MapAdapter.java
@@ -1,29 +1,37 @@
 package com.ystrazhko.git.xml;
 
+import com.ystrazhko.git.entities.Group;
+import com.ystrazhko.git.services.GroupsUserService;
+import com.ystrazhko.git.services.ServiceProvider;
+
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 import java.util.HashMap;
 import java.util.Map;
 
 
-public class MapAdapter extends XmlAdapter<MapElements[], Map<Integer, String>> {
+public class MapAdapter extends XmlAdapter<MapElements[], Map<Group, String>> {
+    private GroupsUserService _groupsService =
+            (GroupsUserService) ServiceProvider.getInstance().getService(GroupsUserService.class.getName());
+
     public MapAdapter() {
     }
 
     @Override
-    public MapElements[] marshal(Map<Integer, String> arg0) throws Exception {
+    public MapElements[] marshal(Map<Group, String> arg0) throws Exception {
         MapElements[] mapElements = new MapElements[arg0.size()];
         int i = 0;
-        for (Map.Entry<Integer, String> entry : arg0.entrySet())
-            mapElements[i++] = new MapElements(entry.getKey(), entry.getValue());
+        for (Map.Entry<Group, String> entry : arg0.entrySet())
+            mapElements[i++] = new MapElements(entry.getKey().getId(), entry.getValue());
 
         return mapElements;
     }
 
     @Override
-    public Map<Integer, String> unmarshal(MapElements[] arg0) throws Exception {
-        Map<Integer, String> r = new HashMap<>();
-        for (MapElements mapelement : arg0)
-            r.put(mapelement.getGroupId(), mapelement.getLocalPath());
+    public Map<Group, String> unmarshal(MapElements[] arg0) throws Exception {
+        Map<Group, String> r = new HashMap<>();
+        for (MapElements mapelement : arg0) {
+            r.put(_groupsService.getGroupById(mapelement.getGroupId()), mapelement.getLocalPath());
+        }
         return r;
     }
 }
