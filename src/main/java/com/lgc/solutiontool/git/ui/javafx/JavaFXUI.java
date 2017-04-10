6601ee77bf3f5d2db8094ea9b4d03c0e11a40b5c
Pavel Podgorniy <podgpavel@gmail.com>
2017-04-10 11:55:58 +0300

    Changed logics of login dialog

--- src/main/java/com/lgc/solutiontool/git/ui/javafx/JavaFXUI.java
+++ src/main/java/com/lgc/solutiontool/git/ui/javafx/JavaFXUI.java
@@ -9,7 +9,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 import javafx.util.Pair;
 
 public class JavaFXUI extends Application implements UserInterface {
@@ -24,6 +23,7 @@ public class JavaFXUI extends Application implements UserInterface {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        showloginDialog();
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("WelcomeWindow.fxml"));
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getClassLoader().getResource("style.css").toExternalForm());
@@ -31,13 +31,12 @@ public class JavaFXUI extends Application implements UserInterface {
         primaryStage.setScene(scene);
         primaryStage.setHeight(800);
         primaryStage.setWidth(1200);
-        primaryStage.setOnShowing(this::login);
         primaryStage.show();
         Label userLabel = (Label) scene.lookup("#userId");
         userLabel.setText(_loginService.getCurrentUser().getName());
     }
 
-    private void login(WindowEvent windowEvent) {
+    private void showloginDialog() {
         LoginDialog ld = new LoginDialog();
         Pair<String, String> loginAndPassword = ld.showAndWait().orElseThrow(() -> new RuntimeException("Error in LoginDialog"));
         _loginService.login(loginAndPassword.getKey(), loginAndPassword.getValue());
--- src/main/java/com/lgc/solutiontool/git/ui/javafx/controllers/WelcomeWindowController.java
+++ src/main/java/com/lgc/solutiontool/git/ui/javafx/controllers/WelcomeWindowController.java
@@ -2,19 +2,23 @@ package com.lgc.solutiontool.git.ui.javafx.controllers;
 
 import com.lgc.solutiontool.git.properties.ProgramProperties;
 import com.lgc.solutiontool.git.entities.Group;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.stage.WindowEvent;
 import javafx.util.Callback;
 
 import java.io.IOException;
@@ -28,6 +32,11 @@ public class WelcomeWindowController {
     private static final String WINDOW_TITLE = "Cloning window";
     @FXML
     public ListView groupList;
+
+    @FXML
+    public Button onLoadSelectedGroupspaceButton;
+
+    public Stage welcomeWindow;
 
     @FXML
     public void onCreateGroupspace(ActionEvent actionEvent) throws IOException {
@@ -98,6 +107,22 @@ public class WelcomeWindowController {
         controller.setSelectedGroup(selectedGroup);
         controller.beforeShowing();
 
+        Stage previousStage = (Stage) onLoadSelectedGroupspaceButton.getScene().getWindow();
+        previousStage.close();
+
+        stage.setOnHiding(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                Platform.runLater(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        previousStage.show();
+                        stage.close();
+                    }
+                });
+            }
+        });
         stage.show();
     }
 }
