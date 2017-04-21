7e22bd7a806a59f81fa2d3469988d2dd30145c46
Lyudmila Lyska <lyudmila.lyska@gmail.com>
2017-04-21 12:35:11 +0300

    operations_with_branches: Added operations with branches getting all branches in the local repository, creating and deleting branches. Switch between branches. Edited JGitStatus.

--- src/main/java/com/lgc/solutiontool/git/jgit/JGit.java
+++ src/main/java/com/lgc/solutiontool/git/jgit/JGit.java
@@ -12,6 +12,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+import org.eclipse.jgit.api.CheckoutCommand;
+import org.eclipse.jgit.api.CreateBranchCommand;
+import org.eclipse.jgit.api.CreateBranchCommand.SetupUpstreamMode;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.MergeResult;
 import org.eclipse.jgit.api.PullResult;
@@ -20,10 +23,22 @@ import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.InvalidRemoteException;
 import org.eclipse.jgit.api.errors.TransportException;
 import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.dircache.DirCache;
+import org.eclipse.jgit.dircache.DirCacheCheckout;
+import org.eclipse.jgit.errors.AmbiguousObjectException;
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.MissingObjectException;
 import org.eclipse.jgit.errors.NoWorkTreeException;
+import org.eclipse.jgit.errors.RevisionSyntaxException;
+import org.eclipse.jgit.lib.AnyObjectId;
+import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectReader;
+import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
+import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.treewalk.CanonicalTreeParser;
 
 import com.lgc.solutiontool.git.entities.Group;
@@ -34,7 +49,9 @@ import com.lgc.solutiontool.git.services.ServiceProvider;
 /**
  * Class for work with Git:
  *
- * [+] clone a group, project or URL of repository; - create repository; - commit; - push.
+ * - clone a group, project or URL of repository;
+ * - pull, commit and push of projects;
+ * - create, delete and switch to branch.
  *
  * @author Lyska Lyudmila
  */
@@ -112,6 +129,18 @@ public class JGit {
             Repository repository = git.getRepository();
             Status status = git.status().call();
             if (status != null) {
+                // debug code
+                System.out.println();
+                System.out.println("Conflicting: " + status.getConflicting());
+                System.out.println("Changed: " + status.getChanged());
+                System.out.println("Added: " + status.getAdded());
+                System.out.println("Ignored Not In Index: " + status.getIgnoredNotInIndex());
+                System.out.println("Conflicting Stage State: " + status.getConflictingStageState());
+                System.out.println("Missing: " + status.getMissing());
+                System.out.println("Modified: " + status.getModified());
+                System.out.println("Untracked: " + status.getUntracked());
+                System.out.println("Untracked Folders: " + status.getUntrackedFolders());
+
                 repository.close();
                 return Optional.of(status);
             }
@@ -170,8 +199,9 @@ public class JGit {
      *
      * @param groupFolderPath the path to cloned of the group
      * @param message for commit
-     * @return true - if the operation is completed successfully, false - if an error occurred during execution !
-     *         Projects that failed to commit will be displayed in the console.
+     * @return true  - if the operation is completed successfully,
+     *         false - if an error occurred during execution
+     * ! Projects that failed to commit will be displayed in the UI console.
      */
     public boolean commit (String groupFolderPath, String message) {
         List<Path> projects = getFilesInFolder(groupFolderPath);
@@ -202,9 +232,9 @@ public class JGit {
      *
      * @param groupFolderPath the path to cloned of the group
      * @param message for commit
-     * @return true - if the operation is completed successfully,
-     * false - if an error occurred during execution
-     * ! Projects that failed to commit or to push will be displayed in the console.
+     * @return true  - if the operation is completed successfully,
+     *         false - if an error occurred during execution
+     * ! Projects that failed to commit or to push will be displayed in the UI console.
      */
     public boolean commitAndPush (String groupFolderPath, String message) {
         List<Path> projects = getFilesInFolder(groupFolderPath);
@@ -234,8 +264,9 @@ public class JGit {
      * Push of all the projects in the group
      *
      * @param groupFolderPath the path to cloned of the group
-     * @return true - if the operation is completed successfully, false - if an error occurred during execution !
-     *         Projects that failed to push will be displayed in the console.
+     * @return true  - if the operation is completed successfully,
+     *         false - if an error occurred during execution
+     * !Projects that failed to push will be displayed in the UI console.
      */
     public boolean push (String groupFolderPath) {
         List<Path> projects = getFilesInFolder(groupFolderPath);
@@ -261,7 +292,116 @@ public class JGit {
         return true;
     }
 
-    private boolean commit (Path projectPath, String message) {
+    /**
+     * Gets all the branch names that are in the local repository
+     *
+     * @param projectPath the path to cloned of the project
+     * @return a list short names of branches
+     */
+    public List<String> getBranches(String projectPath) {
+        return getListShortNamesOfBranches(getRefs(projectPath));
+    }
+
+    /**
+     * Create a new branch in the local repository.
+     *
+     * @param projectPath  the path to cloned of the project
+     * @param nameBranch   the name of the branch
+     * @param force        if <code>true</code> and the branch with the given name
+     *                     already exists, the start-point of an existing branch will be
+     *                     set to a new start-point; if false, the existing branch will
+     *                     not be changed
+     * @return JGitStatus: SUCCESSFUL - if a new branch was created,
+     *                     FAILED - if the branch could not be created.
+     */
+    public JGitStatus createBranch(String projectPath, String nameBranch, boolean force) {
+        Optional<Git> optGit = getGitForRepository(projectPath);
+        if (!optGit.isPresent() || nameBranch == null) { // TODO valid name
+            return JGitStatus.FAILED;
+        }
+        if (!force && getBranches(projectPath).contains(nameBranch)) {
+            System.err.println("!ERROR: a branch with the same name already exists");
+            return JGitStatus.FAILED;
+        }
+        try {
+            CreateBranchCommand create = optGit.get().branchCreate();
+            Ref res = create.setUpstreamMode(SetupUpstreamMode.TRACK).setName(nameBranch)
+                    .setStartPoint(optGit.get().getRepository().getFullBranch()).setForce(force).call();
+            System.out.println("!CREATE NEW BRANCH: " + res.getName());
+            return JGitStatus.SUCCESSFUL;
+        } catch (GitAPIException | IOException e) {
+            System.err.println("!ERROR: " + e.getMessage());
+        }
+        return JGitStatus.FAILED;
+    }
+
+    /**
+     * Switch to another branch (already existing).
+     *
+     * @param projectPath  the path to cloned of the project
+     * @param nameBranch   the name of the branch to which to switch
+     *
+     * @return JGitStatus: SUCCESSFUL - if a new branch was created,
+     *                     FAILED - if the branch could not be created,
+     *                     CONFLICTS - if the branch has unsaved changes that can lead to conflicts.
+     */
+    public JGitStatus switchTo(String projectPath, String nameBranch) {
+        Optional<Git> optGit = getGitForRepository(projectPath);
+        if (!optGit.isPresent() || nameBranch == null) { // TODO valid name
+            return JGitStatus.FAILED;
+        }
+        if (!getBranches(projectPath).contains(nameBranch)) {
+            System.err.println("!ERROR: a branch with this name does not exist.");
+            return JGitStatus.FAILED;
+        }
+        Git git = optGit.get();
+        try {
+            if (isCurrentBranch(git, nameBranch)) {
+                return JGitStatus.FAILED;
+            }
+            if (isConflictsBetweenTwoBranches(git.getRepository(), git.getRepository().getFullBranch(),
+                    Constants.R_HEADS + nameBranch)) {
+                return JGitStatus.CONFLICTS;
+            }
+            CheckoutCommand command = git.checkout();
+            Ref ref = command.setName(nameBranch).setStartPoint("origin/" + nameBranch).call();
+            System.out.println("!Switch to branch: " + ref.getName()); // TODO data to the UI console
+            return JGitStatus.SUCCESSFUL;
+        } catch (IOException | GitAPIException e) {
+            System.err.println("!ERROR: " + e.getMessage());
+        }
+        return JGitStatus.FAILED;
+    }
+
+    /**
+     * Removes a branch by name.
+     *
+     * @param projectPath  the path to cloned of the project
+     * @param nameBranch   the name of the branch for delete
+     * @param force        false - a check will be performed whether the branch to be deleted is already
+     *                     merged into the current branch and deletion will be refused in this case.
+     */
+    public JGitStatus deleteBranch(String projectPath, String nameBranch, boolean force) {
+        Optional<Git> optGit = getGitForRepository(projectPath);
+        if (!optGit.isPresent() || nameBranch == null) {
+            return JGitStatus.FAILED;
+        }
+        Git git = optGit.get();
+        if (isCurrentBranch(git, nameBranch)) {
+            System.err.println("!ERROR: The current branch can not be deleted.");
+            return JGitStatus.FAILED;
+        }
+        try {
+            git.branchDelete().setBranchNames(nameBranch).setForce(force).call();
+            System.out.println("!Branch \"" + nameBranch + "\" deleted from the " + projectPath);
+            return JGitStatus.SUCCESSFUL;
+        } catch (GitAPIException e) {
+            System.err.println("!ERROR: " + e.getMessage());
+        }
+        return JGitStatus.FAILED;
+    }
+
+    private boolean commit(Path projectPath, String message) {
         if (projectPath == null) {
             return false;
         }
@@ -317,8 +457,7 @@ public class JGit {
 
     private Optional<Git> getGitForRepository(String path) {
         try {
-            Git git = Git.open(new File(path + "/.git"));
-            return Optional.ofNullable(git);
+            return Optional.ofNullable(Git.open(new File(path + "/.git")));
         } catch (IOException e) {
             System.err.println("!ERROR: " + e.getMessage());
         }
@@ -377,9 +516,7 @@ public class JGit {
             newTreeIter.reset(reader, fetchHead);
 
             return Optional.ofNullable(git.diff().setNewTree(newTreeIter).call());
-        } catch (IOException e) {
-            System.err.println("!ERROR: " + e.getMessage());
-        }  catch (GitAPIException e) {
+        } catch (IOException | GitAPIException e) {
             System.err.println("!ERROR: " + e.getMessage());
         }
         return Optional.empty();
@@ -400,7 +537,81 @@ public class JGit {
         return false;
     }
 
-    // debug code
+    private List<Ref> getRefs(String projectPath) {
+        Optional<Git> optGit = getGitForRepository(projectPath);
+        if (!optGit.isPresent()) {
+            return Collections.emptyList();
+        }
+        try {
+            return optGit.get().branchList().call();
+        } catch (GitAPIException e) {
+            System.err.println("!ERROR: " + e.getMessage());
+        }
+        return Collections.emptyList();
+    }
+
+    private List<String> getListShortNamesOfBranches(List<Ref> listRefs) {
+        if (listRefs == null) {
+            return Collections.emptyList();
+        }
+        List<String> refs = new ArrayList<>();
+        for (Ref ref : listRefs) {
+            refs.add(ref.getName().substring(Constants.R_HEADS.length()));
+        }
+        return refs;
+    }
+
+    private boolean isConflictsBetweenTwoBranches(Repository repo, String firstBranch, String secondBranch) {
+        if (repo == null) {
+            return false;
+        }
+        try {
+            Ref firstRef = repo.exactRef(firstBranch);
+            Ref secondRef = repo.exactRef(secondBranch);
+            if (firstRef == null || secondRef == null) {
+                return false;
+            }
+
+            RevWalk revWalk = new RevWalk(repo);
+            AnyObjectId headId = firstRef.getObjectId();
+
+            RevCommit firstRefCommit = revWalk.parseCommit(headId);
+            RevCommit secondRefCommit = revWalk.parseCommit(secondRef.getObjectId());
+            revWalk.close();
+
+            if (firstRefCommit == null || secondRefCommit == null) {
+                return false;
+            }
+
+            RevTree headTree = firstRefCommit.getTree();
+            DirCache dirCache = repo.lockDirCache();
+
+            DirCacheCheckout dirCacheCheck = new DirCacheCheckout(repo, headTree, dirCache, secondRefCommit.getTree());
+            dirCacheCheck.setFailOnConflict(true);
+            dirCacheCheck.checkout();
+            dirCache.unlock();
+        } catch (RevisionSyntaxException | IncorrectObjectTypeException | AmbiguousObjectException
+                | MissingObjectException e) {
+            System.err.println("!ERROR: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("!ERROR: " + e.getMessage());
+        }
+        return true;
+    }
+
+    private boolean isCurrentBranch(Git git, String nameBranch) {
+        try {
+            String currentBranch = git.getRepository().getFullBranch();
+            String newBranch = Constants.R_HEADS + nameBranch;
+            if (currentBranch.equals(newBranch)) {
+                return true;
+            }
+        } catch (IOException e) {
+            System.err.println("!ERROR: " + e.getMessage());
+        }
+        return false;
+    }
+
     private Collection<Project> getProjects(Group group) {
         return ((ProjectService) ServiceProvider.getInstance()
                 .getService(ProjectService.class.getName())).getProjects(group);
