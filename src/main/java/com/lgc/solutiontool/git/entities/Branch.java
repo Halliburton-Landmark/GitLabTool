e3e6cc7cff04dfc6c256313dd88f06d3d085b12f
Pavel Podgorniy <podgpavel@gmail.com>
2017-05-23 09:21:11 +0300

    Fixed duplicated branches

--- src/main/java/com/lgc/solutiontool/git/entities/Branch.java
+++ src/main/java/com/lgc/solutiontool/git/entities/Branch.java
@@ -1,6 +1,8 @@
 package com.lgc.solutiontool.git.entities;
 
 import com.lgc.solutiontool.git.jgit.BranchType;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 
 /**
  * Class keeps data about branch.
@@ -46,5 +48,33 @@ public class Branch {
      */
     public BranchType getBranchType() {
         return branchType;
+    }
+
+    private void setBranchType(BranchType bType) {
+        branchType = bType;
+    }
+
+    private void setBranchName(BranchType bName) {
+        branchType = bName;
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 31).
+                append(branchName).
+                toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Branch))
+            return false;
+        if (obj == this)
+            return true;
+
+        Branch rhs = (Branch) obj;
+        return new EqualsBuilder().
+                append(branchName, rhs.branchName).
+                isEquals();
     }
 }
