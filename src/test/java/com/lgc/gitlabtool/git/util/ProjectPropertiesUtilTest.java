83f869009c4f5effeac80be0ade5da3ee4a51d46
Yevhen Strazhko <strazhkoe.dev@gmail.com>
2017-06-22 17:00:16 +0300

    Fixed tests

--- src/test/java/com/lgc/gitlabtool/git/util/ProjectPropertiesUtilTest.java
+++ src/test/java/com/lgc/gitlabtool/git/util/ProjectPropertiesUtilTest.java
@@ -2,48 +2,17 @@ package com.lgc.gitlabtool.git.util;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Properties;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class ProjectPropertiesUtilTest {
 
-    private static final String TEST_SOURCE_PATH = "src/main/resources"; // do not work with File.separator
-    private static final String TEST_RESOURCE_FILE_NAME = "test.properties";
-
-    @Before
-    public void setUp() {
-        Properties props = new Properties();
-        try (FileOutputStream fos = new FileOutputStream(TEST_SOURCE_PATH + File.separator + TEST_RESOURCE_FILE_NAME)) {
-            props.setProperty("gitlabtool.version", "0.0.1");
-            props.setProperty("gitlabtool.name", "Gitlab Tool");
-            props.store(fos, null);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @After
-    public void cleanUp() {
-        try {
-            File file = new File(TEST_SOURCE_PATH + File.separator + TEST_RESOURCE_FILE_NAME);
-            if (file.exists()) {
-                file.delete();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    private static final String EXPECTED_NAME = "Gitlab Tool";
+    private static final String EXPECTED_VERSION = "0.0.1";
+    private static final String TEST_RESOURCE_FILE_NAME = "pom.properties";
 
     @Test
     public void checkGetProjectVersion() {
-        String expectedVersion = "0.0.1";
+        String expectedVersion = EXPECTED_VERSION;
         String actualVersion = ProjectPropertiesUtil.getProjectVersion();
 
         assertEquals(expectedVersion, actualVersion);
@@ -51,16 +20,16 @@ public class ProjectPropertiesUtilTest {
 
     @Test
     public void checkGetProjectName() {
-        String expectedProjectName = "gitlabtool";
+        String expectedProjectName = EXPECTED_NAME;
         String actualProjectName = ProjectPropertiesUtil.getProjectName();
 
         assertEquals(expectedProjectName, actualProjectName);
     }
 
-    @Test @Ignore // TODO: have a problem with resources in jar. I'll think about it later
+    @Test  // TODO: have a problem with resources in jar. I'll think about it later
     public void checkGetRightProperty() {
-        String expectedVersion = "0.0.1";
-        String expectedName = "Gitlab Tool";
+        String expectedVersion = EXPECTED_VERSION;
+        String expectedName = EXPECTED_NAME;
 
         String actualVersion = ProjectPropertiesUtil.getProperty(TEST_RESOURCE_FILE_NAME, "gitlabtool.version");
         String actualName = ProjectPropertiesUtil.getProperty(TEST_RESOURCE_FILE_NAME, "gitlabtool.name");
@@ -69,7 +38,7 @@ public class ProjectPropertiesUtilTest {
         assertEquals(expectedName, actualName);
     }
 
-    @Test @Ignore // TODO: have a problem with resources in jar. I'll think about it later
+    @Test // TODO: have a problem with resources in jar. I'll think about it later
     public void checkGetWrongProperty() {
         String extpectedResult = "undefined";
         String actualResult = ProjectPropertiesUtil.getProperty(TEST_RESOURCE_FILE_NAME, "foo");
