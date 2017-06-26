646f515f513381f56a0f000efc92a71a793d5da4
Igor Khlaponin <igor.boxmails@gmail.com>
2017-06-26 12:02:51 +0300

    added message to CreateNewBranchDialog for the wrong input

--- src/main/java/com/lgc/gitlabtool/git/ui/javafx/CreateNewBranchDialog.java
+++ src/main/java/com/lgc/gitlabtool/git/ui/javafx/CreateNewBranchDialog.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.lgc.gitlabtool.git.entities.Project;
 import com.lgc.gitlabtool.git.jgit.JGitStatus;
 import com.lgc.gitlabtool.git.services.GitService;
@@ -32,10 +35,14 @@ public class CreateNewBranchDialog extends Dialog<String> {
     private static final String DIALOG_TITLE = "Create new branch";
     private static final String STATUS_DIALOG_TITLE = "Branch Creating status";
     private static final String STATUS_DIALOG_HEADER = "Branch creating info";
+    private static final String CHOOSE_BRANCH_NAME_MESSAGE = "Please choose a new branch name";
+    private static final String WRONG_INPUT_MESSAGE = " is a wrong branch name. Please try again!";
 
+    private static final Logger _logger = LogManager.getLogger(CreateNewBranchDialog.class);
     private static final GitService _gitService = (GitService) ServiceProvider.getInstance()
             .getService(GitService.class.getName());
 
+    private final Label _messageLabel;
     private final Label _textLabel;
     private final TextField _branchNameField;
     private final CheckBox _checkoutBox;
@@ -60,14 +67,16 @@ public class CreateNewBranchDialog extends Dialog<String> {
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
 
+        _messageLabel = new Label(CHOOSE_BRANCH_NAME_MESSAGE);
+        grid.add(_messageLabel, 0, 1, 3, 1);
         _textLabel = new Label("New branch: ");
-        grid.add(_textLabel, 0, 1);
+        grid.add(_textLabel, 0, 3);
         _branchNameField = new TextField();
         _branchNameField.addEventFilter(KeyEvent.KEY_TYPED, getInputFilter());
 
-        grid.add(_branchNameField, 1, 1, 2, 1);
+        grid.add(_branchNameField, 1, 3, 2, 1);
         _checkoutBox = new CheckBox("Checkout new branch");
-        grid.add(_checkoutBox, 1, 3);
+        grid.add(_checkoutBox, 1, 4);
 
         _createButton = new Button("Create Branch");
         _createButton.setDisable(true);
@@ -82,7 +91,7 @@ public class CreateNewBranchDialog extends Dialog<String> {
         HBox hbBtn = new HBox(10);
         hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
         hbBtn.getChildren().addAll(_createButton, _cancelButton);
-        grid.add(hbBtn, 2, 5);
+        grid.add(hbBtn, 2, 6);
 
         getDialogPane().setContent(grid);
         Image appIcon = AppIconHolder.getInstance().getAppIcoImage();
@@ -101,6 +110,7 @@ public class CreateNewBranchDialog extends Dialog<String> {
     private void onCreateButton(ActionEvent event) {
         String newBranchName = _branchNameField.getText().trim();
         if (!isInputValid(newBranchName)) {
+            showMessage(_branchNameField.getText() + WRONG_INPUT_MESSAGE);
             return;
         }
         Map<Project, JGitStatus> results = _gitService.createBranch(getProjects(), newBranchName, false);
@@ -136,10 +146,17 @@ public class CreateNewBranchDialog extends Dialog<String> {
         return keyEvent -> {
             if (isInputValid(_branchNameField.getText().trim())) {
                 _createButton.setDisable(false);
+                showMessage(CHOOSE_BRANCH_NAME_MESSAGE);
             } else {
+                showMessage(_branchNameField.getText() + WRONG_INPUT_MESSAGE);
                 _createButton.setDisable(true);
             }
         };
+    }
+
+    private void showMessage(String message) {
+        _messageLabel.setText(message);
+        _logger.debug(message);
     }
 
     private void createAndShowStatusDialog(List<Project> projects, Map<Project, JGitStatus> results) {
