8921542dea58ad15e1edaeb03821761652319574
Lyudmila Lyska <lyudmila.lyska@gmail.com>
2017-06-30 11:09:58 +0300

    parallel_stream_to_switch_to: modified the Git Service class.

--- src/main/java/com/lgc/gitlabtool/git/services/GitServiceImpl.java
+++ src/main/java/com/lgc/gitlabtool/git/services/GitServiceImpl.java
@@ -2,7 +2,6 @@ package com.lgc.gitlabtool.git.services;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -18,12 +17,14 @@ import com.lgc.gitlabtool.git.jgit.JGitStatus;
 
 public class GitServiceImpl implements GitService {
 
+    private static final JGit _git = JGit.getInstance();
+
     @Override
     public boolean containsBranches(Project project, List<Branch> branches, boolean isCommon) {
         if (project == null || branches == null) {
             throw new IllegalArgumentException("Wrong parameters for obtaining branches.");
         }
-        List<Branch> projectBranches = JGit.getInstance().getBranches(project, BranchType.ALL);
+        List<Branch> projectBranches = _git.getBranches(project, BranchType.ALL);
 
         return isCommon ? projectBranches.containsAll(branches) : !Collections.disjoint(projectBranches, branches);
     }
@@ -39,9 +40,8 @@ public class GitServiceImpl implements GitService {
     @Override
     public Map<Project, JGitStatus> switchTo(List<Project> projects, String branchName, boolean isRemote) {
         final Map<Project, JGitStatus> switchStatuses = new ConcurrentHashMap<>();
-        projects.parallelStream()
-                .forEach((project) ->
-                    switchStatuses.put(project, JGit.getInstance().switchTo(project, branchName, isRemote)));
+        projects.parallelStream().forEach(
+                (project) -> switchStatuses.put(project, _git.switchTo(project, branchName, isRemote)));
         return switchStatuses;
     }
 
@@ -52,14 +52,12 @@ public class GitServiceImpl implements GitService {
         }
 
         List<Project> changedProjects = new ArrayList<>();
-
         for (Project project : projects) {
-            Optional<Status> status = JGit.getInstance().getStatusProject(project);
+            Optional<Status> status = _git.getStatusProject(project);
             if (status.isPresent() && status.get().hasUncommittedChanges()) {
                 changedProjects.add(project);
             }
         }
-
         return changedProjects;
     }
 
@@ -68,45 +66,46 @@ public class GitServiceImpl implements GitService {
         if (projects == null) {
             throw new IllegalArgumentException("Wrong parameters for discarding projects.");
         }
-
-        Map<Project, JGitStatus> results = new LinkedHashMap<>();
-        for (Project project : projects) {
-            if(project == null){
-                results.put(project, JGitStatus.FAILED);
-                continue;
-            }
-            JGitStatus status = JGit.getInstance().discardChanges(project);
-            results.put(project, status);
-        }
-
+        final Map<Project, JGitStatus> results = new ConcurrentHashMap<>();
+        projects.parallelStream().forEach(project -> discardChanges(project, results));
         return results;
     }
 
     @Override
-    public Map<Project, JGitStatus> commitChanges(List<Project> projects, String commitMessage, boolean isPushImmediately,
-                    ProgressListener progressListener) {
-        Map<Project, JGitStatus> results;
-        if (isPushImmediately) {
-            //use null for getting default user-info
-            results = JGit.getInstance().commitAndPush(projects, commitMessage, true, null,
-                    null, null, null, progressListener);
-        } else {
-            //use null for getting default user-info
-            results = JGit.getInstance().commit(projects, commitMessage, true, null,
-                    null, null, null, progressListener);
-        }
+    public Map<Project, JGitStatus> commitChanges(List<Project> projects, String commitMessage,
+            boolean isPushImmediately, ProgressListener progressListener) {
 
-        return results;
+        return isPushImmediately ? commitAndPush(projects, commitMessage, progressListener)
+                                 : commit(projects, commitMessage, progressListener);
     }
 
     @Override
     public Map<Project, JGitStatus> createBranch(List<Project> projects, String branchName, boolean force) {
         Map<Project, JGitStatus> statuses = new ConcurrentHashMap<>();
-        projects.parallelStream()
-                .filter(prj -> prj.isCloned())
-                .forEach((project) ->
-                        statuses.put(project, JGit.getInstance().createBranch(project, branchName, force)));
+        projects.parallelStream().filter(prj -> prj.isCloned()).forEach(
+                (project) -> statuses.put(project, _git.createBranch(project, branchName, force)));
         return statuses;
     }
 
+    private void discardChanges(Project project, Map<Project, JGitStatus> results) {
+        if (project == null) {
+            results.put(project, JGitStatus.FAILED);
+            return;
+        }
+        results.put(project, _git.discardChanges(project));
+    }
+
+    private Map<Project, JGitStatus> commit(List<Project> projects,
+                                            String commitMessage,
+                                            ProgressListener progressListener) {
+        // use null for getting default user-info
+        return _git.commitAndPush(projects, commitMessage, true, null, null, null, null, progressListener);
+    }
+
+    private Map<Project, JGitStatus> commitAndPush(List<Project> projects,
+                                                   String commitMessage,
+                                                   ProgressListener progressListener) {
+        // use null for getting default user-info
+        return _git.commitAndPush(projects, commitMessage, true, null, null, null, null, progressListener);
+    }
 }
