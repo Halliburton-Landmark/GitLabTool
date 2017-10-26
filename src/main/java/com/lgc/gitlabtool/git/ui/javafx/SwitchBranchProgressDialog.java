c3abf0c6b85b18c0ac0d44628787441a353ea03a
Lyudmila Lyska <lyudmila.lyska@gmail.com>
2017-10-26 10:42:38 +0300

    204_add_progress: moved string to the constant.

--- src/main/java/com/lgc/gitlabtool/git/ui/javafx/SwitchBranchProgressDialog.java
+++ src/main/java/com/lgc/gitlabtool/git/ui/javafx/SwitchBranchProgressDialog.java
@@ -2,9 +2,16 @@ package com.lgc.gitlabtool.git.ui.javafx;
 
 import com.lgc.gitlabtool.git.listeners.stateListeners.ApplicationState;
 
+/**
+ * Realization of progress dialog for the switch branch operation.
+ *
+ * @author Lyudmila Lyska
+ */
 public class SwitchBranchProgressDialog extends ProgressDialog {
 
+    private static final String SWITCH_BRANCH_DIALOG_TITLE = "Switch operation";
+
     public SwitchBranchProgressDialog() {
-        super("Switch operation", ApplicationState.SWITCH_BRANCH, CancelButtonStatus.DEACTIVATED);
+        super(SWITCH_BRANCH_DIALOG_TITLE, ApplicationState.SWITCH_BRANCH, CancelButtonStatus.DEACTIVATED);
     }
 }
