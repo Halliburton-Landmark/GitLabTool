409c7f00c1fe847200201e2cdb8a6baa76d9154b
Igor Khlaponin <igor.boxmails@gmail.com>
2017-12-01 15:31:03 +0200

    #214 - BackgroundService: refactoring
    did ThreadFactory in BackgroundService as simple as possible

--- src/main/java/com/lgc/gitlabtool/git/services/BackgroundServiceImpl.java
+++ src/main/java/com/lgc/gitlabtool/git/services/BackgroundServiceImpl.java
@@ -19,43 +19,21 @@ public class BackgroundServiceImpl implements BackgroundService {
      * Creates the instance of this class
      */
     public BackgroundServiceImpl() {
-        ThreadFactory namingThreadFactory = buildFactory("background-service-%d", false, Thread.NORM_PRIORITY);
+        ThreadFactory namingThreadFactory = getNamingThreadFactory();
         _executorService = Executors.newCachedThreadPool(namingThreadFactory);
     }
 
-    /**
-     * Builds new instance of the {@link ThreadFactory} based on set parameters
-     * @param namingPattern - thread naming pattern
-     * @param isDaemon - {@code true} if all thread created by this {@code TreadFactory} should be daemons,
-     *                   {@code false} otherwise
-     * @param priority - the priority of created threads
-     * @return new instance of the ThreadFactory based on set parameters
-     */
-    private ThreadFactory buildFactory(String namingPattern, boolean isDaemon, int priority) {
+    private ThreadFactory getNamingThreadFactory() {
         final AtomicLong counter = new AtomicLong(0);
         final ThreadFactory factory = Executors.defaultThreadFactory();
         return new ThreadFactory() {
             @Override
             public Thread newThread(Runnable runnable) {
                 Thread thread = factory.newThread(runnable);
-                if (namingPattern != null) {
-                    thread.setName(String.format(namingPattern, counter.getAndIncrement()));
-                }
-                thread.setDaemon(isDaemon);
-                if (validPriority(priority)) {
-                    thread.setPriority(priority);
-                }
+                thread.setName(String.format("background-service-%d", counter.getAndIncrement()));
                 return thread;
             }
         };
-    }
-
-    private boolean validPriority(int priority) {
-        String errorMessage = "Wrong priority! It should be >= " + Thread.MIN_PRIORITY + " and <= " + Thread.MAX_PRIORITY;
-        if (priority < Thread.MIN_PRIORITY || priority > Thread.MAX_PRIORITY) {
-            throw new IllegalArgumentException(errorMessage);
-        }
-        return true;
     }
 
     @Override
