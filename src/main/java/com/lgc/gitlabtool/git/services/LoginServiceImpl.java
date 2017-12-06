5415453f0a6e742601984b842ed591d11cf2db7c
Pavel Podgorniy <podgpavel@gmail.com>
2017-12-06 16:33:59 +0200

    Added login patch from wiki

--- src/main/java/com/lgc/gitlabtool/git/services/LoginServiceImpl.java
+++ src/main/java/com/lgc/gitlabtool/git/services/LoginServiceImpl.java
@@ -10,6 +10,7 @@ import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import com.lgc.gitlabtool.git.connections.HttpResponseHolder;
 import com.lgc.gitlabtool.git.connections.RESTConnector;
 import com.lgc.gitlabtool.git.connections.token.CurrentUser;
+import com.lgc.gitlabtool.git.connections.token.Token;
 import com.lgc.gitlabtool.git.entities.User;
 import com.lgc.gitlabtool.git.ui.javafx.dto.DialogDTO;
 import com.lgc.gitlabtool.git.util.JSONParser;
@@ -29,15 +30,30 @@ public class LoginServiceImpl implements LoginService {
     public void login(DialogDTO dto, Consumer<HttpResponseHolder> onSuccess) {
         Runnable runnable = () -> {
             HashMap<String, String> params = new HashMap<>();
-            params.put("login", dto.getLogin());
+            params.put("grant_type", "password");
+            params.put("username", dto.getLogin());
             params.put("password", dto.getPassword());
-            getConnector().setUrlMainPart(dto.getServerURL());
-            HttpResponseHolder responseHolder = getConnector().sendPost("/session", params, null);
-            Object userJson = responseHolder != null ? responseHolder.getBody() : null;
-            if (isResponseCodeValid(responseHolder, userJson)) {
-                _currentUser = CurrentUser.getInstance();
-                _currentUser.setCurrentUser(JSONParser.parseToObject(userJson, User.class));
-                CredentialsProvider.setDefault(new UsernamePasswordCredentialsProvider(dto.getLogin(), dto.getPassword()));
+
+            getConnector().setUrlMainPart(dto.getShortServerURL());
+            HttpResponseHolder responseHolder = getConnector().sendPost("/oauth/token", params, null);
+            Object tokenJson = responseHolder != null ? responseHolder.getBody() : null;
+            if (isResponseCodeValid(responseHolder, tokenJson)) {
+                Token accessToken = JSONParser.parseToObject(tokenJson, Token.class);
+                if (accessToken != null) {
+                    HashMap<String, String> secureHeader = new HashMap<>();
+                    secureHeader.put("Authorization", accessToken.getTokenWithType());
+                    getConnector().setUrlMainPart(dto.getServerURL());
+                    HttpResponseHolder userInfoResponseHolder = getConnector().sendGet("/user", null, secureHeader);
+                    Object userJson2 = userInfoResponseHolder != null ? userInfoResponseHolder.getBody() : null;
+                    if (isResponseCodeValid(userInfoResponseHolder, userJson2)) {
+                        User newUser = JSONParser.parseToObject(userJson2, User.class);
+                        _currentUser = CurrentUser.getInstance();
+                        _currentUser.setCurrentUser(newUser);
+                        _currentUser.setPrivateTokenValue(accessToken.getTokenWithType());
+                    }
+
+                    CredentialsProvider.setDefault(new UsernamePasswordCredentialsProvider(dto.getLogin(), dto.getPassword()));
+                }
             }
             onSuccess.accept(responseHolder);
         };
@@ -50,11 +66,8 @@ public class LoginServiceImpl implements LoginService {
     }
     
     private boolean isResponseCodeValid(HttpResponseHolder responseHolder, Object userJson) {
-        if (responseHolder.getResponseCode() == HttpStatus.SC_REQUEST_TIMEOUT
-                || responseHolder.getResponseCode() == 0 || userJson == null) {
-            return false;
-        }
-        return true;
+        return !(responseHolder.getResponseCode() == HttpStatus.SC_REQUEST_TIMEOUT
+                || responseHolder.getResponseCode() == 0 || userJson == null);
     }
 
     private RESTConnector getConnector() {
