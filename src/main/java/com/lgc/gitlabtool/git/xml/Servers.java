930b0268ffaddfc395c97b299ab77d950011a8e9
Pavel Podgorniy <podgpavel@gmail.com>
2018-02-15 13:59:24 +0200

    changed order of servers

--- src/main/java/com/lgc/gitlabtool/git/xml/Servers.java
+++ src/main/java/com/lgc/gitlabtool/git/xml/Servers.java
@@ -18,9 +18,9 @@ public class Servers {
 
     public Servers() {
         servers = new ArrayList<>();
+        servers.add(new Server("git.openearth.community", "v3"));
         servers.add(new Server("gitlab.com", "v3"));
         servers.add(new Server("gitlab.lgc.com", "v3"));
-        servers.add(new Server("git.openearth.community", "v3"));
     }
 
     public Servers(List<Server> servers) {
