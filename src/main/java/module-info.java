48ba14d03a41d5d776378a4f01358989ca4c8169
Yevhen Strazhko <strazhkoe.dev@gmail.com>
2018-03-02 15:40:28 +0200

    157: First commit. Added module-info.java and modified some dependencies. Builds only without tests with JDK 9. CI will not build it.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module gitlabtool {
+    requires java.xml.bind;
+    requires javafx.controls;
+    requires log4j.api;
+    requires javafx.fxml;
+    requires org.eclipse.jgit;
+    requires httpcore;
+    requires gson;
+    requires commons.io;
+    requires commons.lang;
+    requires maven.model;
+    requires vtd.xml;
+    requires java.desktop;
+    requires java.prefs;
+    requires plexus.utils;
+
+}
